eb8f9bde29470af50e93ce8a8836d22eb26ec8ea
tan-zhuo <tanzhuo@aliyun.com>
2026-05-27 22:14:07 +0900

    chore: bump pluginVersion to v1.6.1

--- constants.go
+++ constants.go
@@ -3,7 +3,7 @@ package rocketmq
 const (
 	// Plugin metadata
 	pluginName        = "rocketmq"
-	pluginVersion     = "v1.6.0-beta"
+	pluginVersion     = "v1.6.1"
 	pluginDescription = "RocketMQ message queue plugin for Lynx framework"
 	confPrefix        = "rocketmq"
 
